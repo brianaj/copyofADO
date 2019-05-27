8189f05fc33d125759ce78c67cf9b2d5174f0f23
brianaj <brianaj22@gmail.com>
2019-05-27 11:59:26 -0700

    unoptimized array

--- gradebook2/src/Gradebook/Program.cs
+++ gradebook2/src/Gradebook/Program.cs
@@ -6,9 +6,17 @@ namespace gradebook2
     {
         static void Main(string[] args)
         {
+            var numbers = new double[3];
             var x=22.5; //using var (implictily typed) requires intialization
             double y=17.8;
-            var result = x+y;
+            numbers[0] =x;
+            numbers[1] =y;
+            numbers[2] =42.2;
+
+            double result;
+            result = numbers[0] + numbers[1] + numbers[2];
+//            for(int i =0; i<numbers.GetLength; i++)
+
 
             Console.WriteLine($"The result is: {result}");
 
