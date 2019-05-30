87d9570489f6548884073db69904881a2df6f8e4
brianaj <brianaj22@gmail.com>
2019-05-30 12:38:00 -0700

    Writing and running new xunit test

--- gradebook2/test/Gradebook.Tests/UnitTest1.cs
+++ gradebook2/test/Gradebook.Tests/UnitTest1.cs
@@ -5,9 +5,16 @@ namespace Gradebook.Tests
 {
     public class UnitTest1
     {
+
+        //fact is equivalent to @Test
         [Fact]
         public void Test1()
         {
+            var x = 5;
+            var y =2;
+            var expected = 7;
+            var actual = x+y;
+            Assert.Equal(expected, actual);
 
         }
     }
