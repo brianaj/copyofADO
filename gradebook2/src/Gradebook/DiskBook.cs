815cbdd3e494b650c03dae092de08b0e92dee4ad
brianaj <brianaj22@gmail.com>
2019-06-07 21:45:14 -0700

    End of OO module

--- gradebook2/src/Gradebook/DiskBook.cs
+++ gradebook2/src/Gradebook/DiskBook.cs
@@ -7,7 +7,7 @@ namespace Gradebook
     {
         private string v;
 
-        public DiskBook(string v): base(v)
+        public DiskBook(string v) : base(v)
         {
             this.v = v;
         }
@@ -16,21 +16,32 @@ namespace Gradebook
 
         public override void AddGrade(double grade)
         {
-           using(var streamer = File.AppendText( $"/Users/brijohns/DotNetProjects/gradebook2/gradebook2/src/Gradebook/{Name}.txt"))
-           {
-               streamer.WriteLine(grade);
-               if(GradeAdded != null)
-               {
-                   GradeAdded(this, new EventArgs());
-               }
-           }
-           // using statement will invoke Close or Disponse in a finally block
-        //    streamer.Close();
+            using (var streamer = File.AppendText($"/Users/brijohns/DotNetProjects/gradebook2/gradebook2/src/Gradebook/{Name}.txt"))
+            {
+                streamer.WriteLine(grade);
+                if (GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
+            }
+            // using statement will invoke Close or Disponse in a finally block
+            //    streamer.Close();
         }
 
         public override Statistics GetStats()
         {
-            throw new System.NotImplementedException();
+            using (var reader = File.OpenText($"/Users/brijohns/DotNetProjects/gradebook2/gradebook2/src/Gradebook/{Name}.txt"))
+            {
+                Statistics stats = new Statistics();
+                var nextLine = reader.ReadLine();
+                while (nextLine != null)
+                {
+                    stats.Add(double.Parse(nextLine));
+                    nextLine = reader.ReadLine();
+                }
+                return stats;
+            }
+
         }
     }
 }
