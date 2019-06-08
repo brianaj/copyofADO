1d745202e3dee7d3e784f873f8ac8edc96a65008
brianaj <brianaj22@gmail.com>
2019-06-07 20:09:11 -0700

    add event delegate

--- gradebook2/src/Gradebook/DiskBook.cs
+++ gradebook2/src/Gradebook/DiskBook.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 
 namespace Gradebook
@@ -18,6 +19,10 @@ namespace Gradebook
            using(var streamer = File.AppendText( $"/Users/brijohns/DotNetProjects/gradebook2/gradebook2/src/Gradebook/{Name}.txt"))
            {
                streamer.WriteLine(grade);
+               if(GradeAdded != null)
+               {
+                   GradeAdded(this, new EventArgs());
+               }
            }
            // using statement will invoke Close or Disponse in a finally block
         //    streamer.Close();
